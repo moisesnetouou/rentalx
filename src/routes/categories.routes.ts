e7246078cb1d2b2b0f4175d3542f3a3eecc45f99
Moisés <moisesnetored@gmail.com>
2022-03-31 17:47:00 -0400

    feat: uuid added, version in the project uuidV4

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,21 @@
 import { Router } from "express"; // para criar a nossa rota
+import { v4 as uuidV4 } from "uuid";
 
 const categoriesRoutes = Router();
 
 const categories = []; // bd
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   // recurso de categories
   const { name, description } = request.body;
 
-  categories.push({
+  const category = {
+    id: uuidV4(),
     name,
     description,
-  });
+  };
+
+  categories.push(category);
 
   return response.status(201).send();
 });
