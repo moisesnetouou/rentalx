2cc368107bbb7812c00629376d59ecb914faf15a
Moisés <moisesnetored@gmail.com>
2022-04-01 09:34:23 -0400

    feat: Category constructor created.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,25 @@
 import { Router } from "express"; // para criar a nossa rota
-import { v4 as uuidV4 } from "uuid";
+
+import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = []; // bd
+const categories: Category[] = []; // bd
 
 categoriesRoutes.post("/", (request, response) => {
   // recurso de categories
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidV4(),
+  const category = new Category();
+  Object.assign(category, {
     name,
     description,
-  };
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
-  return response.status(201).send();
+  return response.status(201).json({ category });
 });
 
 export { categoriesRoutes };
