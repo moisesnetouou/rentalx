5e72b13b4238c974fdd0e45ef2cbc64981fa0418
Moisés <moisesnetored@gmail.com>
2022-04-08 11:07:14 -0400

    feat: Function to prevent duplication of existing category.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -9,9 +9,21 @@ categoriesRoutes.post("/", (request, response) => {
   // recurso de categories
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category already exists!" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
